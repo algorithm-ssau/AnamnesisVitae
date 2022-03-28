abc1c04b7791a1b46a3bd3bf3a0c543d7320044e
Mikhail Trofimov <easymeowus@gmail.com>
2022-03-28 10:38:20 +0400

    T-0 Added start application method with database connection; Added default config file

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('test run server')
+const express = require("express")
+const config = require("config")
+const mongoose = require("mongoose")
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('mongoURI'))
+    } catch (e) {
+        console.log('Server error', e.message)
+        process.exit(1)
+    }
+}
+
+start()
+
+app.listen(PORT, () => console.log('App has been started'))
