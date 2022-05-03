29d48ee4e71f671f542ccd45c081157141232343
Andrey <kochetpwr@gmail.com>
2022-05-03 19:52:41 +0400

    add ApplicationHelp and upgraded CreatePage

---
+++ frontend/src/components/ApplicationHelp.js
@@ -0,0 +1,20 @@
+export const ApplicationHelp = () => {
+    return (
+        <div className="application-help">
+            <p className="application-help-text">
+                Справка для приложения Anamnesis Vitae<br/>
+                и тут по ходу надо будет накатать справку
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                бла бла бла
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                бла бла бла
+            </p>
+        </div>
+    )
+}
--- frontend/src/pages/CreatePage.js
+++ frontend/src/pages/CreatePage.js
@@ -1,16 +1,61 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Questionnaire } from "../components/Questionnaire";
 import {Navbar} from "../components/Navbar";
+import {ApplicationHelp} from "../components/ApplicationHelp";
 import { useNavigate } from "react-router-dom";
 import {AuthContext} from '../context/AuthContext'
 
 export const CreatePage = () => {
     const history = useNavigate();
     const auth = useContext(AuthContext)
-return (
-    <div className="content-box-create-page">
-        <Navbar/>
-        <Questionnaire auth={auth} history={history}/>
-    </div>
-)
+    const [window,setWindow] = useState(1)
+
+    if (window == 1) {
+        return (
+                <div className="content-box-create-page">
+                    <ul className="navbar-vitae">
+                        <li><a onClick={() => {setWindow(1)}} className="navbar-vitae-a-active">Диагностика</a></li>
+                        <li><a onClick={() => {setWindow(2)}} className="navbar-vitae-a-passive">Мои ресурсы</a></li>
+                        <li><a onClick={() => {setWindow(3)}} className="navbar-vitae-a-passive">Профиль</a></li>
+                        <li><a onClick={() => {setWindow(4)}} className="navbar-vitae-a-passive">Справка</a></li>
+                    </ul>
+                    <Questionnaire auth={auth} history={history}/>
+                </div>
+
+        )
+    } else if (window == 2) {
+        return (
+            <div className="content-box-create-page">
+                <ul className="navbar-vitae">
+                    <li><a onClick={() => {setWindow(1)}} className="navbar-vitae-a-passive">Диагностика</a></li>
+                    <li><a onClick={() => {setWindow(2)}} className="navbar-vitae-a-active">Мои ресурсы</a></li>
+                    <li><a onClick={() => {setWindow(3)}} className="navbar-vitae-a-passive">Профиль</a></li>
+                    <li><a onClick={() => {setWindow(4)}} className="navbar-vitae-a-passive">Справка</a></li>
+                </ul>
+            </div>
+        )
+    } else if (window == 3) {
+        return (
+            <div className="content-box-create-page">
+                <ul className="navbar-vitae">
+                    <li><a onClick={() => {setWindow(1)}} className="navbar-vitae-a-passive">Диагностика</a></li>
+                    <li><a onClick={() => {setWindow(2)}} className="navbar-vitae-a-passive">Мои ресурсы</a></li>
+                    <li><a onClick={() => {setWindow(3)}} className="navbar-vitae-a-active">Профиль</a></li>
+                    <li><a onClick={() => {setWindow(4)}} className="navbar-vitae-a-passive">Справка</a></li>
+                </ul>
+            </div>
+        )
+    } else if (window == 4) {
+        return (
+            <div className="content-box-create-page">
+                <ul className="navbar-vitae">
+                    <li><a onClick={() => {setWindow(1)}} className="navbar-vitae-a-passive">Диагностика</a></li>
+                    <li><a onClick={() => {setWindow(2)}} className="navbar-vitae-a-passive">Мои ресурсы</a></li>
+                    <li><a onClick={() => {setWindow(3)}} className="navbar-vitae-a-passive">Профиль</a></li>
+                    <li><a onClick={() => {setWindow(4)}} className="navbar-vitae-a-active">Справка</a></li>
+                </ul>
+                <ApplicationHelp/>
+            </div>
+        )
+    }
 }
