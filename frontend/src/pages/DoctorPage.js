dfdcb2fb832b2ccfb274e18fd445f0ad557f0c36
Mikhail Trofimov <easymeowus@gmail.com>
2022-05-09 16:52:54 +0400

    Added patients handler to DoctorPage

--- frontend/src/pages/DoctorPage.js
+++ frontend/src/pages/DoctorPage.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, {useContext} from "react";
+import {useHttp} from "../hooks/http.hook";
+import {AuthContext} from "../context/AuthContext";
 
 export const DoctorPage = () => {
+    const { request } = useHttp();
+    const auth = useContext(AuthContext)
+
+    const patientsHandler = async () => {
+        try {
+            const patientsRequest = await request(
+                "/api/auth/patients",
+                "POST",
+                {},
+                { Authorization: `Bearer ${auth.token}` }
+                );
+            console.log(patientsRequest.patients)
+        } catch (error) {
+            console.log(error);
+        }
+     }
+
 return (
     <div className="doctor-page">
         <h1>Doctor Page</h1>
+        {/*<button className="enter-button" onClick={patientsHandler}>Подтвердить</button>*/}
     </div>
 )
 }
